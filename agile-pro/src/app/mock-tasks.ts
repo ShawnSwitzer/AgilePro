517ebc370de5f241e00b728aaab62a3c2fb7d29c
Shawn Switzer <shawnswitz@gmail.com>
2022-04-20 13:57:06 -0500

    delete mock stuff

--- agile-pro/src/app/mock-tasks.ts
+++
@@ -1,6 +0,0 @@
-import { Tasks } from "./tasks";
-
-export const TASKS: Tasks[] = [
-    {id: 1, title: 'Create Component', description: 'Create component in angular project.'},
-    {id: 2, title: 'Delete Component', description: 'Delete component in angular project.'}
-]
--- agile-pro/src/app/mock-userstories.ts
+++
@@ -1,6 +0,0 @@
-import { UserStories } from "./userstories";
-
-export const USERSTORIES: UserStories[] = [
-    {id: 1, description: 'As a User, I want to ...' },
-    {id: 2, description: 'As a manager, I want to...' }
-]
