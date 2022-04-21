24cd8da701d56948bb5d46bad6c4f3c3b168a18b
Shawn Switzer <shawnswitz@gmail.com>
2022-04-21 01:08:17 -0500

    Update memberstorage.service.ts

--- agile-pro/src/app/memberstorage.service.ts
+++ agile-pro/src/app/memberstorage.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { Members } from './members';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemberstorageService {
 
-  constructor() { }
+  constructor(private http: HttpClient ) { }
 }
