4182e36c4699d1350236d83775a6f1ed63b93fc7
Shawn Switzer <shawnswitz@gmail.com>
2022-04-21 23:35:28 -0500

    fixed blank page bug

--- agile-pro/src/app/app.module.ts
+++ agile-pro/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,11 +20,13 @@ import { MemberInputComponent } from './member-input/member-input.component';
     TaskComponent,
     NewUserStoryComponent,
     MemberInputComponent,
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    HttpClientModule
   
   ],
   providers: [],
