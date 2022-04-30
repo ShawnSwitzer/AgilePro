fa9bea6c6ba9a1661f2e662461bdfe80b5927baa
Shawn Switzer <shawnswitz@gmail.com>
2022-04-30 16:26:25 -0500

    set up router links

--- agile-pro/src/app/app-routing.module.ts
+++ agile-pro/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { MemberInputComponent } from './member-input/member-input.component';
+import { NewUserStoryComponent } from './new-user-story/new-user-story.component';
+import { TaskComponent } from './task/task.component';
+import { UserStoryComponent } from './user-story/user-story.component';
 
 const routes: Routes = [
+  {path: 'members', component: MemberInputComponent},
+  {path: 'userstories', component: UserStoryComponent},
+  {path: 'newuserstory', component: NewUserStoryComponent},
+  {path: 'tasks', component: TaskComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: '', redirectTo: '/dashboard', pathMatch: 'full' },
 ];
 
 @NgModule({
