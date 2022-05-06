035104042e2995f0c34861e005071a3049734b0c
tg5788re <jfokugas@gmail.com>
2022-05-05 22:08:33 -0500

    Updated CSS
    Updated CSS of user/new-user stories components

--- agile-pro/src/app/app.module.ts
+++ agile-pro/src/app/app.module.ts
@@ -20,6 +20,7 @@ import { MatSliderModule } from '@angular/material/slider';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
+
 @NgModule ({
   declarations: [
     AppComponent,
