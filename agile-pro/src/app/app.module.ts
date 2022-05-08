8db1ee85b3ea1a6a74fd0c626befe6491ef2074d
Shawn Switzer <shawnswitz@gmail.com>
2022-05-08 01:45:26 -0500

    fix refresh page 404 error

--- agile-pro/src/app/app.module.ts
+++ agile-pro/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -48,7 +49,7 @@ import { provideDatabase,getDatabase } from '@angular/fire/database';
     provideDatabase(() => getDatabase())
   
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass: HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
